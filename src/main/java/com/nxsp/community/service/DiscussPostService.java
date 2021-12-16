4b97fa4dfdbe9cd4b37cda4eb4383ea47d7549f4
nx <1539853554@qq.com>
2021-12-16 18:40:03 +0800

    上传社区交流项目

---
+++ src/main/java/com/nxsp/community/service/DiscussPostService.java
@@ -0,0 +1,24 @@
+package com.nxsp.community.service;
+
+import com.nxsp.community.dao.DiscussPostMapper;
+import com.nxsp.community.entity.DiscussPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiscussPostService {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
+        return discussPostMapper.selectDiscussPosts(userId, offset, limit);
+    }
+
+    public int findDiscussPostRows(int userId) {
+        return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+}
