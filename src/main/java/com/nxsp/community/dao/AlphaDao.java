19c9c95ce8edfa85c85834a7604623031c94dcee
nx <1539853554@qq.com>
2021-12-25 22:21:09 +0800

    发布帖子

---
+++ src/main/java/com/nxsp/community/dao/AlphaDao.java
@@ -0,0 +1,10 @@
+package com.nxsp.community.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlphaDao {
+
+    String select();
+
+}
--- src/main/java/com/nxsp/community/service/DiscussPostService.java
+++ src/main/java/com/nxsp/community/service/DiscussPostService.java
@@ -2,8 +2,10 @@ package com.nxsp.community.service;
 
 import com.nxsp.community.dao.DiscussPostMapper;
 import com.nxsp.community.entity.DiscussPost;
+import com.nxsp.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 @Service
@@ -12,12 +14,34 @@ public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
 
     public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
+    }
+    public int addDiscussPost(DiscussPost post){
+        if(post==null){
+            throw new IllegalArgumentException("参数不能为空");
+        }
+        // 转义HTML标记(恶意注册的时候，会使用诸如 <script>alert('papapa')</script>，转义标签)
+        //敏感词过滤
+        //转义HTML标记
+        post.setTitle(sensitiveFilter.filter(HtmlUtils.htmlEscape(post.getTitle())));
+        post.setContent(sensitiveFilter.filter(HtmlUtils.htmlEscape(post.getContent())));
+        return discussPostMapper.insertDiscussPost(post);
+    }
+
+    public DiscussPost findDiscussPostById(int id) {
+        return discussPostMapper.selectDiscussPostById(id);
+    }
+
+    public int updateCommentCount(int id, int commentCount) {
+        return discussPostMapper.updateCommentCount(id, commentCount);
     }
 
 }
